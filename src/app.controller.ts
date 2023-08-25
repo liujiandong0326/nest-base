222978bb6a503b40f69d25b19673981ab3f59f23
liujd2 <liujiandong0326@163.com>
2023-08-25 15:29:53 +0800

    feat: 初始化完成

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    console.log('123');
+    console.log('222');
+    return this.appService.getHello();
+  }
+}
